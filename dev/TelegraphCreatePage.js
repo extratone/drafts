5c89eab263ac93c214f1ea5cc5915255865237a5
David Blue <davidblue@extratone.com>
2022-04-28 11:10:07 -0500

    Telegraph??

---
+++ dev/TelegraphCreatePage.js
@@ -0,0 +1,46 @@
+// setup and request credentials
+let credential = Credential.create("Telegraph", "Enter your Telegraph username and auth token.");
+
+credential.addTextField("account", "Account");
+credential.addPasswordField("token", "Access Token");
+credential.authorize();
+
+const token = credential.getValue("token");
+const collection = credential.getValue("account");
+
+// make post
+let endpoint = `https://write.as/api/collections/${collection}/posts`;
+
+https://api.telegra.ph/createPage?access_token=ecaa11b2eba4e4fa89a641d86d29415acb89c19cf232b8eb2d54642fec49&title=Sample+Page&author_name=Anonymous&content=[{"tag":"p","children":["Hello,+world!"]}]&return_content=tru
+
+// configure post details
+// for more information, see:
+// https://developers.write.as/docs/api
+let data = {
+	"body": draft.content,
+};
+
+// create and post HTTP request
+var http = HTTP.create();
+
+var response = http.request({
+	"url": endpoint,
+	"method": "POST",
+	"data": data,
+	"headers": {
+		"Authorization" : token,
+	}
+});
+
+// log result post URL and copy to clipboard
+if (response.statusCode == 201) {
+	let d = JSON.parse(response.responseText);
+	console.log("Posted to Writeas. URL: " + d.data.url);
+	app.setClipboard(d.data.url);
+	draft.setTemplateTag("walink", d.data.url);
+}
+
+else {
+	console.log("Write.as Error: " + response.error);
+	context.fail();
+}
