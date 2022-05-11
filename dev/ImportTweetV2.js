30a8f72345e6bff7e51a9ba25600e861ebb97295
David Blue <davidblue@extratone.com>
2022-05-10 22:37:31 -0500

    #56

--- dev/ImportTweetV2.js
+++ dev/ImportTweetV2.js
@@ -1,3 +1,14 @@
+// setup and request credentials
+let credential = Credential.create("TwitterV2", "Enter your Twitter credentials");
+
+credential.addTextField("username", "Twitter username");
+credential.addPasswordField("token", "Twitter API Bearer token");
+credential.authorize();
+
+const uname = credential.getValue("username");
+const token = credential.getValue("token");
+
+
 var twitter = Twitter.create();
 
 let selected = editor.getSelectedRange()[1] > 0 ? editor.getSelectedText() : editor.getText();
@@ -10,12 +21,21 @@ var baseURL = "https://api.twitter.com/2/tweets/${id}";
 
 var params = "attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,referenced_tweets,reply_settings,source,text,withheld";
 
-var head = "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAOu5bgEAAAAAx4lokpJ8YEiLuAomAmrkBSr4Jv4%3DtzbfrrT0JeGuIg8Asc806dgiWWtcAUIRZTb3w9EtOd2Ica2DWs";
+var head = "Authorization: Bearer ${token}";
 
+/*
 var response = twitter.request({
 	"url": baseURL,
 	"method": "GET",
     "headers:": head,
+    "parameters": params,
+});
+*/
+
+var response = http.request({
+    "url": baseURL,
+    "method": "GET",
+    "headers": head,
     "parameters": params,
 });
 
