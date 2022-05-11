d80724b33ceaf02343772717822a121ea6044356
David Blue <davidblue@extratone.com>
2022-05-10 22:03:15 -0500

    #56

--- dev/ImportTweetV2.js
+++ dev/ImportTweetV2.js
@@ -2,24 +2,24 @@ var twitter = Twitter.create();
 
 let selected = editor.getSelectedRange()[1] > 0 ? editor.getSelectedText() : editor.getText();
 
-let mdlink = [`[Tweet](${selected})`,""];
+/*let mdlink = [`[Tweet](${selected})`,""]; */
 
-var id = selected => selected.substring(selected.lastIndexOf('/') + 1);
+var id = selected.substring(selected.lastIndexOf('/') + 1);
 
-var baseURL = "https://api.twitter.com/1.1/";
-var url = baseURL + "statuses/show.json";
+var baseURL = "https://api.twitter.com/2/tweets/${id}";
+
 
 var response = twitter.request({
-	"url": url,
+	"url": baseURL,
 	"method": "GET",
-    "parameters": {"id": id,}
+    "parameters": {"tweet.fields": "attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,referenced_tweets,reply_settings,source,text,withheld"}
 });
 
 if (response.statusCode == 200 || response.statusCode == 201) {
     let text = response.responseData.text;
-    mdlink.push(`${text}\n${mdlink}`);
+ /*   mdlink.push(`${text}\n${mdlink}`); */
     let d = Draft.create();
-    d.content = mdlink.join("\n");
+    d.content = text;
     d.update();
     editor.load(d);
     console.log("Tweet retrieved:" + text);
